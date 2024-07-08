31737c80d230783b6df77d3f8fc4336613ec056b
David Moseler <davidmoseler@protonmail.com>
2024-07-08 04:35:42 -0300

    Add repost confirmation modal

--- frontend/src/components/Post.tsx
+++ frontend/src/components/Post.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { dependencyContainer, useDependencies } from '../dependencyContainer';
 import { useRepost } from '../repositories/postRepository';
 import TPost from '../types/post';
 import TUser from '../types/user';
 import { RootState } from '../store';
+import Modal from 'react-modal';
 
 dependencyContainer.register('Post', ({ post }: { post: TPost }) => {
   const users = useSelector((state: RootState) => state.user.users);
@@ -16,28 +17,32 @@ dependencyContainer.register('Post', ({ post }: { post: TPost }) => {
 
 const Post = ({ post }: { post: TPost }) => {
   const { users, repost } = useDependencies('Post', { post });
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const getUserName = (user_id: string) => {
     const user = users.find((u: TUser) => u.id == user_id);
     return user ? user.name : '';
   };
 
+  const confirmRepost = () => {
+    repost();
+    setIsModalOpen(false);
+  };
+
   return (
     <div className="post border-b border-gray-200 p-4">
       {post.repost ? (
-        <p>{getUserName(post.reposter_id) + ' reposted'}</p>
-      ) : (
-        <></>
-      )}
+        <p>{`${getUserName(post.reposter_id)} reposted`}</p>
+      ) : null}
       <h2>
         <span className="font-bold">{getUserName(post.user_id)}</span>
-        <span className="pl-3 text-gray-400">{post.created_at.split('T')[0]}</span>
+        <span className="pl-3 text-gray-400">
+          {post.created_at.split('T')[0]}
+        </span>
       </h2>
       <p>{post.content}</p>
-      {post.repost ? (
-        <></>
-      ) : (
-        <button data-testid="repost" onClick={() => repost()}>
+      {!post.repost && (
+        <button data-testid="repost" onClick={() => setIsModalOpen(true)}>
           <svg
             className="text-teal-600"
             width="20px"
@@ -47,22 +52,38 @@ const Post = ({ post }: { post: TPost }) => {
             strokeWidth="2"
             fill="none"
           >
-            {' '}
-            <title id="repostIconTitle">Repost</title>{' '}
-            <path d="M13 18L6 18L6 7" /> <path d="M3 9L6 6L9 9" />{' '}
-            <path d="M11 6L18 6L18 17" /> <path d="M21 15L18 18L15 15" />{' '}
+            <title id="repostIconTitle">Repost</title>
+            <path d="M13 18L6 18L6 7" />
+            <path d="M3 9L6 6L9 9" />
+            <path d="M11 6L18 6L18 17" />
+            <path d="M21 15L18 18L15 15" />
           </svg>
-          {/* <svg
-            viewBox="0 0 24 24"
-            aria-hidden="true"
-            className="r-4qtqp9 r-yyyyoo r-dnmrzs r-bnwqim r-lrvibr r-m6rgpd r-1xvli5t r-1hdv0qi"
-          >
-            <g>
-              <path d="M4.5 3.88l4.432 4.14-1.364 1.46L5.5 7.55V16c0 1.1.896 2 2 2H13v2H7.5c-2.209 0-4-1.79-4-4V7.55L1.432 9.48.068 8.02 4.5 3.88zM16.5 6H11V4h5.5c2.209 0 4 1.79 4 4v8.45l2.068-1.93 1.364 1.46-4.432 4.14-4.432-4.14 1.364-1.46 2.068 1.93V8c0-1.1-.896-2-2-2z"></path>
-            </g>
-          </svg> */}
         </button>
       )}
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={() => setIsModalOpen(false)}
+        contentLabel="Confirm Repost"
+        ariaHideApp={false}
+        className="rounded-xl m-auto h-36 w-1/3 bg-white fixed inset-0 flex flex-col items-center justify-center p-2 z-50 "
+        overlayClassName="fixed inset-0 bg-black bg-opacity-10"
+      >
+        <h2 className="m-1">Are you sure you want to repost?</h2>
+        <div className="p-3 flex">
+          <button
+            className="mx-3 bg-blue-400 text-white w-16 h-9 rounded-3xl"
+            onClick={confirmRepost}
+          >
+            Yes
+          </button>
+          <button
+            className="mx-3 bg-blue-400 text-white w-16 h-9 rounded-3xl"
+            onClick={() => setIsModalOpen(false)}
+          >
+            No
+          </button>
+        </div>
+      </Modal>
     </div>
   );
 };
